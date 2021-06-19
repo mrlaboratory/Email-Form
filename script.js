67b37f9cde746ef7bc4aeab3fc6257d949b824a2
mrlaboratory <mrlaboratory23@gmail.com>
2021-06-19 14:30:43 +0600

    uptade

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
-
-
 $(".sbutton").on("click",function(){
     var adminemail = "admin@mrlaboratory.com";
     var eName = $("input.input.name").val();
     var eEmail = $("input.input.email").val();
     var eWebsite = $("input.input.website").val();
-    var eTitle = $("input.input.title").val();
-    var eContent = $("input.input.content").val();
+    var eTitle = $("textarea.input.title").val();
+    var eContent = $("textarea.input.content").val();
     var aemail =eName + eEmail+eWebsite+eTitle+eContent;
-var posttitleLink ="mailto:"+adminemail+"?Subject="+eTitle+"&body="+eContent;
-link = posttitleLink.toString();
-window.location.href = posttitleLink;
+var adminemail = "admin@mrlaboratory.com";
+var emailformmr ="mailto:"+adminemail+"?Subject="+eTitle+"&body="+eContent;
+window.location.href = emailformmr;
 })
+
 
